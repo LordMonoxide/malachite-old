62cb343ea2c35f1ec1efc96a0ea08a3517fcbb3c
Corey Frenette <corey@narwhunderful.com>
2013-04-28 22:07:56 -0300

    GUI: scroll panel test

--- malachite/src/game/graphics/gui/Menu.java
+++ malachite/src/game/graphics/gui/Menu.java
@@ -3,11 +3,13 @@ package game.graphics.gui;
 import graphics.shared.gui.GUI;
 import graphics.shared.gui.controls.Dropdown;
 import graphics.shared.gui.controls.Picture;
+import graphics.shared.gui.controls.compound.ScrollPanel;
 import graphics.shared.textures.Texture;
 
 public class Menu extends GUI {
   private Picture  picTest;
   private Dropdown drpTest;
+  private ScrollPanel splTest;
   
   public void load() {
     _context.setBackColour(new float[] {1, 1, 1, 1});
@@ -23,8 +25,11 @@ public class Menu extends GUI {
     drpTest.add("Test 2");
     drpTest.setSeletected(0);
     
+    splTest = new ScrollPanel(this);
+    
     Controls().add(picTest);
     Controls().add(drpTest);
+    Controls().add(splTest);
   }
   
   public void destroy() {
