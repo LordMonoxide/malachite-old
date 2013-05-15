87a81fef6d1545e195cda12f89c1107c9193594a
Corey Frenette <corey@narwhunderful.com>
2013-05-15 20:16:35 -0300

    Message: new GUI

---
+++ client/malachite/src/game/graphics/gui/Message.java
@@ -0,0 +1,95 @@
+package game.graphics.gui;
+
+import graphics.gl00.Context;
+import graphics.shared.gui.Control;
+import graphics.shared.gui.GUI;
+import graphics.shared.gui.controls.Button;
+import graphics.shared.gui.controls.Label;
+import graphics.shared.gui.controls.compound.Window;
+
+public class Message extends GUI {
+  private Context _context;
+  private Window _window;
+  private Label _text;
+  private Button _okay;
+  
+  public static Message show(String text) {
+    return show(null, text);
+  }
+  
+  public static Message show(String title, String text) {
+    Message g = new Message(title, text);
+    g.push();
+    return g;
+  }
+  
+  public static Message showWait(String text) {
+    Message g = show(text);
+    g._okay.setVisible(false);
+    return g;
+  }
+  
+  public static Message showWait(String title, String text) {
+    Message g = show(text);
+    g._okay.setVisible(false);
+    return g;
+  }
+  
+  public Message() {
+    this(null, null);
+  }
+  
+  public Message(String text) {
+    this(null, text);
+  }
+  
+  public Message(String title, String text) {
+    _context = Context.getContext();
+    _text = new Label(this);
+    
+    _okay = new Button(this);
+    _okay.setText("Okay");
+    _okay.setFocus(true);
+    _okay.events().onClick(new Control.Events.Click() {
+      public void event() {
+        pop();
+      }
+    });
+    
+    _window = new Window(this);
+    _window.setText(title);
+    _window.Controls().add(_text);
+    _window.Controls().add(_okay);
+    _window.events().onClose(new Window.Events.Close() {
+      public boolean event() {
+        pop();
+        return true;
+      }
+    });
+    
+    Controls().add(_window);
+    
+    setText(text);
+  }
+  
+  public void setText(String text) {
+    _text.setText(text);
+    _window.setWH(_text.getW() + _window.getW() - _window.getClientW() + 30, _okay.getH() + 80);
+    _window.setXY((int)(_context.getW() - _window.getW()) / 2, (int)(_context.getH() - _window.getH()) / 2);
+    _text.setXY((_window.getClientW() - _text.getW()) / 2, (_window.getClientH() - _text.getH() - _okay.getH()) / 2);
+    _okay.setW(_window.getClientW());
+    _okay.setY(_window.getClientH() - _okay.getH());
+  }
+  
+  public void load() {
+    
+  }
+  
+  public void destroy() {
+    
+  }
+  
+  public void resize() {
+    
+  }
+}
