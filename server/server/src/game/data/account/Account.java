a4f9cf44562825591af5bed84da5ad21e919e5d9
Corey Frenette <corey@narwhunderful.com>
2013-05-17 20:57:17 -0300

    Server: store ID and permissions in account

--- server/server/src/game/data/account/Account.java
+++ server/server/src/game/data/account/Account.java
@@ -1,7 +1,16 @@
 package game.data.account;
 
 public class Account {
+  private int _id;
   private String _name;
+  
+  private Permissions _permissions;
+  
+  public Account(int id) {
+    _id = id;
+  }
+  
+  public int getID() { return _id; }
   
   public String getName() {
     return _name;
@@ -9,5 +18,13 @@ public class Account {
   
   public void setName(String name) {
     _name = name;
+  }
+  
+  public Permissions getPermissions() {
+    return _permissions;
+  }
+  
+  public void setPermissions(Permissions permissions) {
+    _permissions = permissions;
   }
 }
