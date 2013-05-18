a3da8ea8c5c1c4823ad431f5e917f7b332d20dfa
Corey Frenette <corey@narwhunderful.com>
2013-05-18 15:49:16 -0300

    Server: rename Player to Character

--- server/server/src/game/data/account/Character.java
+++ server/server/src/game/data/account/Character.java
@@ -1,6 +1,6 @@
 package game.data.account;
 
-public class Player {
+public class Character {
   private int _id;
   private Account _account;
   private String _name;
@@ -8,7 +8,7 @@ public class Player {
   private float _x, _y;
   private int _z;
   
-  public Player(int id, Account account) {
+  public Character(int id, Account account) {
     _id = id;
     _account = account;
   }
@@ -21,9 +21,11 @@ public class Player {
   public float   getY()       { return _y; }
   public int     getZ()       { return _z; }
   
-  public void setName (String name)  { _name = name; }
-  public void setWorld(String world) { _world = world; }
-  public void setX(float x) { _x = x; }
-  public void setY(float y) { _y = y; }
-  public void setZ(int z)   { _z = z; }
+  public void setID     (int id)          { _id = id; }
+  public void setAccount(Account account) { _account = account; }
+  public void setName   (String name)     { _name = name; }
+  public void setWorld  (String world)    { _world = world; }
+  public void setX      (float x)         { _x = x; }
+  public void setY      (float y)         { _y = y; }
+  public void setZ      (int z)           { _z = z; }
 }
