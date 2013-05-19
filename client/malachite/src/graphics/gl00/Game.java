d17d4badd844683bada70f7e18c566f2e98cce96
Corey Frenette <corey@narwhunderful.com>
2013-05-19 09:43:16 -0300

    Move client to it's own repo

--- client/malachite/src/graphics/gl00/Game.java
+++
@@ -1,6 +0,0 @@
-package graphics.gl00;
-
-public interface Game {
-  public void init();
-  public void destroy();
-}
--- client/malachite/src/graphics/gl00/Scalable.java
+++
@@ -1,5 +0,0 @@
-package graphics.gl00;
-
-public abstract class Scalable extends Drawable {
-  public abstract void setSize(float[] st, float[] sl, float tw, float th, float ts);
-}
