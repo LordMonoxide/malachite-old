dc4e109c75a14e7d43a8c83eed0a53a8e1ef4e26
Corey Frenette <corey@narwhunderful.com>
2013-06-07 09:54:19 -0300

    Register new Save packets

--- code/src/game/network/Client.java
+++ code/src/game/network/Client.java
@@ -8,7 +8,7 @@ import game.network.packet.EntityCreate;
 import game.network.packet.EntityDestroy;
 import game.network.packet.EntityMoveStart;
 import game.network.packet.EntityMoveStop;
-import game.network.packet.editors.SaveMap;
+import game.network.packet.editors.Save;
 import game.network.packet.menu.CharDel;
 import game.network.packet.menu.CharNew;
 import game.network.packet.menu.CharUse;
@@ -54,10 +54,11 @@ public class Client {
     Packets.add(Data.Request.class);
     Packets.add(Data.Response.class);
     Packets.add(Chat.class);
-    Packets.add(SaveMap.class);
+    Packets.add(Save.MapData.class);
     Packets.add(Data.MapRequest.class);
     Packets.add(Data.Info.class);
     Packets.add(Data.MapResponse.class);
+    Packets.add(Save.SpriteData.class);
   }
   
   public void connect() {
