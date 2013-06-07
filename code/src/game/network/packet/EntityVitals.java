579ca8d03b49449b340e7f959ab5297c7605a065
Corey Frenette <corey@narwhunderful.com>
2013-06-07 15:53:40 -0300

    EntityVitals packet

---
+++ code/src/game/network/packet/EntityVitals.java
@@ -0,0 +1,29 @@
+package game.network.packet;
+
+import game.Game;
+import game.world.Entity;
+import io.netty.buffer.ByteBuf;
+import network.packet.Packet;
+
+public class EntityVitals extends Packet {
+  public int getIndex() {
+    return 22;
+  }
+  
+  public ByteBuf serialize() {
+    return null;
+  }
+  
+  public void deserialize(ByteBuf data) throws NotEnoughDataException {
+    Entity e = Game.getInstance().getWorld().getEntity(data.readInt());
+    
+    for(int i = 0; i < Entity.Stats.VITALS; i++) {
+      e.stats().vital(i).val = data.readInt();
+      e.stats().vital(i).max = data.readInt();
+    }
+  }
+  
+  public void process() {
+    
+  }
+}
