13c41c285b14b012d32af823c27aa14f09fda22a
Corey Frenette <corey@narwhunderful.com>
2013-06-09 14:42:44 -0300

    Create ItemEditor

---
+++ code/src/game/graphics/gui/editors/ItemEditor.java
@@ -0,0 +1,34 @@
+package game.graphics.gui.editors;
+
+import game.data.util.Data;
+import graphics.shared.gui.GUI;
+
+public class ItemEditor extends GUI implements Editor {
+  public void load() {
+    
+  }
+  
+  public void destroy() {
+    
+  }
+  
+  public void resize() {
+    
+  }
+  
+  public void draw() {
+    
+  }
+  
+  public boolean logic() {
+    return false;
+  }
+  
+  public void newData(String file) {
+    
+  }
+  
+  public void editData(Data data) {
+    
+  }
+}
