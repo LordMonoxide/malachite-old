236129cbf04033fe7237b1694b710639362c4a46
Corey Frenette <corey@narwhunderful.com>
2013-06-09 14:42:39 -0300

    Create ItemEditorItem

---
+++ code/src/game/graphics/gui/editors/ItemEditorItem.java
@@ -0,0 +1,33 @@
+package game.graphics.gui.editors;
+
+import game.data.Item;
+import game.data.util.Buffer;
+
+public class ItemEditorItem extends Item {
+  private Item _item;
+  private int _itemCRC;
+  
+  public ItemEditorItem(Item item) {
+    super(item.getFile(), item.getCRC());
+    
+    _item = item;
+    
+    // Deep-copy source Item into
+    // this ItemEditorItem's structure
+    Buffer b = _item.serialize();
+    deserialize(b);
+    _itemCRC = b.crc();
+  }
+  
+  public boolean isChanged() {
+    return _itemCRC != serialize().crc();
+  }
+  
+  public void update() {
+    _item.deserialize(serialize());
+  }
+  
+  public Item getItem() {
+    return _item;
+  }
+}
