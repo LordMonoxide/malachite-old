e54eece9d99699b60ffa7f6bb22309120d5a8ce1
Corey Frenette <corey@narwhunderful.com>
2013-06-13 15:44:32 -0300

    Receive entity type and use byte for Z

--- code/src/game/network/packet/EntityCreate.java
+++ code/src/game/network/packet/EntityCreate.java
@@ -24,12 +24,13 @@ public class EntityCreate extends Packet {
     _entity = new Entity();
     _entity.setID(data.readInt());
     _entity.setName(new String(data.readBytes(data.readShort()).array()));
+    _entity.setType(Entity.Type.valueOf(data.readInt()));
     _entity.setSprite(Game.getInstance().getSprite(new String(data.readBytes(data.readShort()).array())));
     _entity.setAcc(data.readFloat());
     _entity.setDec(data.readFloat());
     _entity.setVelTerm(data.readFloat());
     _entity.setInitialXY(data.readFloat(), data.readFloat());
-    _entity.setZ(data.readInt());
+    _entity.setZ(data.readByte());
   }
   
   public void process() {
