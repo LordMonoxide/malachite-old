a9b5164304cd96dbb1b9bdae4c72628a323846b5
Corey Frenette <corey@narwhunderful.com>
2013-06-13 21:46:29 -0300

    EntityInteract packet

---
+++ code/src/game/network/packet/EntityInteract.java
@@ -0,0 +1,32 @@
+package game.network.packet;
+
+import game.world.Entity;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import network.packet.Packet;
+
+public class EntityInteract extends Packet {
+  private Entity _entity;
+  
+  public EntityInteract(Entity entity) {
+    _entity = entity;
+  }
+  
+  public int getIndex() {
+    return 27;
+  }
+  
+  public ByteBuf serialize() {
+    ByteBuf b = Unpooled.buffer();
+    b.writeInt(_entity.getID());
+    return b;
+  }
+  
+  public void deserialize(ByteBuf data) throws NotEnoughDataException {
+    
+  }
+  
+  public void process() {
+    
+  }
+}
