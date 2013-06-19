81339becd98e02c54a345387f897db4b75b70104
Corey Frenette <corey@narwhunderful.com>
2013-06-19 16:45:28 -0300

    Add slot to InvUse, to determine which hand

--- code/src/game/network/packet/InvUse.java
+++ code/src/game/network/packet/InvUse.java
@@ -1,5 +1,6 @@
 package game.network.packet;
 
+import game.data.Item;
 import game.world.Entity;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -7,9 +8,12 @@ import network.packet.Packet;
 
 public class InvUse extends Packet {
   private Entity.Inv _inv;
+  private int _slot;
   
-  public InvUse(Entity.Inv inv) {
+  public InvUse(Entity.Inv inv) { this(inv, 0); }
+  public InvUse(Entity.Inv inv, int slot) {
     _inv = inv;
+    _slot = slot;
   }
   
   public int getIndex() {
@@ -19,6 +23,13 @@ public class InvUse extends Packet {
   public ByteBuf serialize() {
     ByteBuf b = Unpooled.buffer();
     b.writeInt(_inv.index());
+    
+    switch(_inv.item().getType() & Item.ITEM_TYPE_BITMASK) {
+      case Item.ITEM_TYPE_WEAPON:
+      case Item.ITEM_TYPE_SHIELD:
+        b.writeByte(_slot);
+    }
+    
     return b;
   }
   
