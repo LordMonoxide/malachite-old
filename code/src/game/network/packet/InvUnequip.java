5b7afec3da3b269339a146da879e17c6bce0e6b4
Corey Frenette <corey@narwhunderful.com>
2013-06-20 14:59:31 -0300

    InvUnequip packet

---
+++ code/src/game/network/packet/InvUnequip.java
@@ -0,0 +1,37 @@
+package game.network.packet;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import network.packet.Packet;
+
+public class InvUnequip extends Packet {
+  public static final int HAND   = 0;
+  public static final int ARMOUR = 1;
+  public static final int BLING  = 2;
+  
+  private int _type, _slot;
+  
+  public InvUnequip(int type, int slot) {
+    _type = type;
+    _slot = slot;
+  }
+  
+  public int getIndex() {
+    return 32;
+  }
+  
+  public ByteBuf serialize() {
+    ByteBuf b = Unpooled.buffer();
+    b.writeByte(_type);
+    b.writeByte(_slot);
+    return b;
+  }
+  
+  public void deserialize(ByteBuf data) throws NotEnoughDataException {
+    
+  }
+  
+  public void process() {
+    
+  }
+}
