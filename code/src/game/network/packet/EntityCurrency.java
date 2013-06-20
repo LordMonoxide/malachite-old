319112b1151249b59df949abd8092457559c411e
Corey Frenette <corey@narwhunderful.com>
2013-06-20 16:51:15 -0300

    EntityCurrency packet

---
+++ code/src/game/network/packet/EntityCurrency.java
@@ -0,0 +1,29 @@
+package game.network.packet;
+
+import game.Game;
+import game.world.Entity;
+import io.netty.buffer.ByteBuf;
+import network.packet.Packet;
+
+public class EntityCurrency extends Packet {
+  private int _id;
+  private long _curr;
+  
+  public int getIndex() {
+    return 33;
+  }
+  
+  public ByteBuf serialize() {
+    return null;
+  }
+  
+  public void deserialize(ByteBuf data) throws NotEnoughDataException {
+    _id = data.readInt();
+    _curr = data.readLong();
+  }
+  
+  public void process() {
+    Entity e = Game.getInstance().getWorld().getEntity(_id);
+    e.currency(_curr);
+  }
+}
