21008f6e8b80dd7ef34960c31bdc56998c1ca26b
Corey Frenette <corey@narwhunderful.com>
2013-06-20 11:51:15 -0300

    InvDrop packet

---
+++ code/src/game/network/packet/InvDrop.java
@@ -0,0 +1,33 @@
+package game.network.packet;
+
+import game.world.Entity;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import network.packet.Packet;
+
+public class InvDrop extends Packet {
+  private Entity.Inv _inv;
+  
+  public InvDrop(Entity.Inv inv) {
+    _inv = inv;
+  }
+  
+  public int getIndex() {
+    return 31;
+  }
+  
+  public ByteBuf serialize() {
+    ByteBuf b = Unpooled.buffer();
+    b.writeInt(_inv.index());
+    b.writeInt(_inv.val());
+    return b;
+  }
+  
+  public void deserialize(ByteBuf data) throws NotEnoughDataException {
+    
+  }
+  
+  public void process() {
+    
+  }
+}
