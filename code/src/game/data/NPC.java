9e0617ca08f9c585687fee1cecab8adcf05ac3d4
Corey Frenette <corey@narwhunderful.com>
2013-06-25 19:05:43 -0300

    Add NPC class

---
+++ code/src/game/data/NPC.java
@@ -0,0 +1,30 @@
+package game.data;
+
+import java.io.File;
+
+import game.data.util.Buffer;
+import game.data.util.GameData;
+
+public class NPC extends GameData {
+  protected String _sprite;
+  
+  public NPC(String file) {
+    super(1, new File("../data/NPCs/" + file));
+  }
+  
+  public String getSprite() { return _sprite; }
+  
+  protected void serializeInternal(Buffer b) {
+    
+  }
+  
+  protected void deserializeInternal(Buffer b) {
+    switch(getVersion()) {
+      case 1: deserialize01(b); break;
+    }
+  }
+  
+  private void deserialize01(Buffer b) {
+    
+  }
+}
