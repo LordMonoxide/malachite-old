9001e273fe7a7af932ef9fec7c1c60462cf7d02f
Corey Frenette <corey@narwhunderful.com>
2013-06-26 16:20:16 -0300

    Editor Data packet for getting full GameData structures

---
+++ code/src/game/network/packet/editors/EditorData.java
@@ -0,0 +1,90 @@
+package game.network.packet.editors;
+
+import game.Game;
+import game.data.Item;
+import game.data.NPC;
+import game.data.Sprite;
+import game.data.util.Buffer;
+import game.data.util.GameData;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import network.packet.Packet;
+
+public class EditorData {
+  public static final byte DATA_TYPE_SPRITE = 1;
+  public static final byte DATA_TYPE_ITEM = 2;
+  public static final byte DATA_TYPE_NPC = 3;
+  
+  public static class Request extends Packet {
+    private byte _type;
+    private String _file;
+    
+    public Request(GameData data) {
+      if(data instanceof Sprite) _type = DATA_TYPE_SPRITE;
+      if(data instanceof Item)   _type = DATA_TYPE_ITEM;
+      if(data instanceof NPC)    _type = DATA_TYPE_NPC;
+      
+      _file = data.getFile();
+    }
+    
+    public int getIndex() {
+      return 36;
+    }
+    
+    public ByteBuf serialize() {
+      ByteBuf b = Unpooled.buffer();
+      b.writeByte(_type);
+      b.writeShort(_file.length());
+      b.writeBytes(_file.getBytes());
+      return b;
+    }
+    
+    public void deserialize(ByteBuf data) throws NotEnoughDataException {
+      
+    }
+    
+    public void process() {
+      
+    }
+  }
+  
+  public static class Response extends Packet {
+    private byte _type;
+    private String _file;
+    private byte[] _data;
+    
+    public int getIndex() {
+      return 37;
+    }
+    
+    public String getFile() {
+      return _file;
+    }
+    
+    public ByteBuf serialize() {
+      return null;
+    }
+    
+    public void deserialize(ByteBuf data) throws NotEnoughDataException {
+      _type = data.readByte();
+      byte[] arr = new byte[data.readShort()];
+      data.readBytes(arr);
+      _file = new String(arr);
+      _data = new byte[data.readInt()];
+      data.readBytes(_data);
+    }
+    
+    public void process() {
+      GameData data = null;
+      
+      switch(_type) {
+        case DATA_TYPE_SPRITE: data = Game.getInstance().getSprite(_file); break;
+        case DATA_TYPE_ITEM:   data = Game.getInstance().getItem(_file);   break;
+        case DATA_TYPE_NPC:    data = Game.getInstance().getNPC(_file);    break;
+      }
+      
+      data.deserialize(new Buffer(_data));
+      data.save();
+    }
+  }
+}
