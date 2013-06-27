9cec23135361b3f1b7761b2755d166cce0bfe81e
Corey Frenette <corey@narwhunderful.com>
2013-06-27 11:23:24 -0300

    Syncing of full structures from server

--- code/src/game/graphics/gui/editors/ItemEditorItem.java
+++ code/src/game/graphics/gui/editors/ItemEditorItem.java
@@ -1,34 +1,29 @@
 package game.graphics.gui.editors;
 
+import game.Game;
 import game.data.Item;
 import game.data.util.Buffer;
+import game.network.packet.Data;
+import game.network.packet.editors.EditorData;
+import graphics.gl00.Context;
 
 public class ItemEditorItem extends Item {
-  private Item _item;
   private int _itemCRC;
   
-  public ItemEditorItem(Item item) {
-    super(item.getFile());
+  public ItemEditorItem(String file) { this(file, false); }
+  public ItemEditorItem(String file, boolean newData) {
+    super(file);
     
-    _item = item;
-    
-    // Deep-copy source Item into
-    // this ItemEditorItem's structure
-    Buffer b = _item.serialize();
-    deserialize(b);
-    _itemCRC = b.crc();
+    if(!newData) {
+      request();
+    } else {
+      _loaded = true;
+      _events.raiseLoad();
+    }
   }
   
   public boolean isChanged() {
     return _itemCRC != serialize().crc();
-  }
-  
-  public void update() {
-    _item.deserialize(serialize());
-  }
-  
-  public Item getItem() {
-    return _item;
   }
   
   public void setName  (String name)   { _name = name; }
@@ -39,4 +34,37 @@ public class ItemEditorItem extends Item {
   public void setWeight(float weight)  { _weight = weight; }
   public void setHPHeal(int hpHeal)    { _hpHeal = hpHeal; }
   public void setMPHeal(int mpHeal)    { _mpHeal = mpHeal; }
+  
+  public void request() {
+    Data.Request p = new Data.Request(this);
+    Game.getInstance().send(p, EditorData.Response.class, new Game.PacketCallback<EditorData.Response>() {
+      public boolean recieved(final EditorData.Response packet) {
+        if(packet.getType() == EditorData.DATA_TYPE_ITEM && packet.getFile().equals(getFile())) {
+          remove();
+          
+          Context.getContext().addLoadCallback(new Context.Loader.Callback() {
+            public void load() {
+              deserialize(new Buffer(packet.getData()));
+              
+              System.out.println("Full item " + getFile() + " synced from server");
+              _loaded = true;
+              _events.raiseLoad();
+            }
+          }, false, "itemeditoritem");
+          
+          return true;
+        }
+        
+        return false;
+      }
+    });
+  }
+  
+  public void serializeInternal(Buffer b) {
+    super.serializeInternal(b);
+  }
+  
+  public void deserializeInternal(Buffer b) {
+    super.deserializeInternal(b);
+  }
 }
--- code/src/game/graphics/gui/editors/SpriteEditorSprite.java
+++ code/src/game/graphics/gui/editors/SpriteEditorSprite.java
@@ -2,64 +2,72 @@ package game.graphics.gui.editors;
 
 import java.util.ArrayList;
 
+import game.Game;
 import game.data.Sprite;
 import game.data.util.Buffer;
+import game.network.packet.editors.EditorData;
+import graphics.gl00.Context;
 
 public class SpriteEditorSprite extends Sprite {
-  private Sprite _sprite;
   private int _spriteCRC;
   protected ArrayList<Frame> _frame = super._frame;
   protected ArrayList<Anim>  _anim  = super._anim;
   
-  public SpriteEditorSprite(Sprite sprite) {
-    super(sprite.getFile());
+  public SpriteEditorSprite(String file) { this(file, false); }
+  public SpriteEditorSprite(String file, boolean newData) {
+    super(file);
     
-    _sprite = sprite;
-    
-    // Deep-copy source Sprite into
-    // this SpriteEditorSprite's structure
-    Buffer b = _sprite.serialize();
-    deserialize(b);
-    _spriteCRC = b.crc();
+    if(!newData) {
+      request();
+    } else {
+      _loaded = true;
+      _events.raiseLoad();
+    }
   }
   
   public boolean isChanged() {
     return _spriteCRC != serialize().crc();
   }
   
-  public void update() {
-    _sprite.deserialize(serialize());
+  public String getTexture() { return _texture; }
+  
+  public void setName(String name) { _name = name; }
+  public void setNote(String note) { _note = note; }
+  public void setW(int w) { _w = w; }
+  public void setH(int h) { _h = h; }
+  public void setScript(String script) { _script = script; }
+  public void setTexture(String texture) { _texture = texture; }
+  
+  public void request() {
+    EditorData.Request p = new EditorData.Request(this);
+    Game.getInstance().send(p, EditorData.Response.class, new Game.PacketCallback<EditorData.Response>() {
+      public boolean recieved(final EditorData.Response packet) {
+        if(packet.getType() == EditorData.DATA_TYPE_SPRITE && packet.getFile().equals(getFile())) {
+          remove();
+          
+          Context.getContext().addLoadCallback(new Context.Loader.Callback() {
+            public void load() {
+              deserialize(new Buffer(packet.getData()));
+              
+              System.out.println("Full sprite " + getFile() + " synced from server");
+              _loaded = true;
+              _events.raiseLoad();
+            }
+          }, false, "spriteeditorsprite");
+          
+          return true;
+        }
+        
+        return false;
+      }
+    });
   }
   
-  public Sprite getSprite() {
-    return _sprite;
+  public void serializeInternal(Buffer b) {
+    super.serializeInternal(b);
   }
   
-  public void setName(String name) {
-    _name = name;
-  }
-  
-  public void setNote(String note) {
-    _note = note;
-  }
-  
-  public void setW(int w) {
-    _w = w;
-  }
-  
-  public void setH(int h) {
-    _h = h;
-  }
-  
-  public void setScript(String script) {
-    _script = script;
-  }
-  
-  public String getTexture() {
-    return _texture;
-  }
-  
-  public void setTexture(String texture) {
-    _texture = texture;
+  public void deserializeInternal(Buffer b) {
+    super.deserializeInternal(b);
   }
 }
