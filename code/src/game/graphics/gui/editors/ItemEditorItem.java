736733b9fb3731cb372ce3b18faa3c9486b92d08
Corey Frenette <corey@narwhunderful.com>
2013-07-11 14:38:38 -0300

    Protect anything that doesn't need to be public

--- code/src/game/graphics/gui/editors/ItemEditorItem.java
+++ code/src/game/graphics/gui/editors/ItemEditorItem.java
@@ -7,10 +7,8 @@ import game.network.packet.editors.EditorData;
 import graphics.gl00.Context;
 
 public class ItemEditorItem extends Item {
-  private int _itemCRC;
-  
-  public ItemEditorItem(String file) { this(file, false); }
-  public ItemEditorItem(String file, boolean newData) {
+  protected ItemEditorItem(String file) { this(file, false); }
+  protected ItemEditorItem(String file, boolean newData) {
     super(file);
     
     if(!newData) {
@@ -21,18 +19,14 @@ public class ItemEditorItem extends Item {
     }
   }
   
-  public boolean isChanged() {
-    return _itemCRC != serialize().crc();
-  }
-  
-  public void setName  (String name)   { _name = name; }
-  public void setNote  (String note)   { _note = note; }
-  public void setSprite(String sprite) { _sprite = sprite; }
-  public void setType  (int type)      { _type = type; }
-  public void setDamage(int damage)    { _damage = damage; }
-  public void setWeight(float weight)  { _weight = weight; }
-  public void setHPHeal(int hpHeal)    { _hpHeal = hpHeal; }
-  public void setMPHeal(int mpHeal)    { _mpHeal = mpHeal; }
+  protected void setName  (String name)   { _name = name; }
+  protected void setNote  (String note)   { _note = note; }
+  protected void setSprite(String sprite) { _sprite = sprite; }
+  protected void setType  (int type)      { _type = type; }
+  protected void setDamage(int damage)    { _damage = damage; }
+  protected void setWeight(float weight)  { _weight = weight; }
+  protected void setHPHeal(int hpHeal)    { _hpHeal = hpHeal; }
+  protected void setMPHeal(int mpHeal)    { _mpHeal = mpHeal; }
   
   public void request() {
     EditorData.Request p = new EditorData.Request(this);
@@ -59,11 +53,11 @@ public class ItemEditorItem extends Item {
     });
   }
   
-  public void serializeInternal(Buffer b) {
+  protected void serializeInternal(Buffer b) {
     super.serializeInternal(b);
   }
   
-  public void deserializeInternal(Buffer b) {
+  protected void deserializeInternal(Buffer b) {
     super.deserializeInternal(b);
   }
 }
--- code/src/game/graphics/gui/editors/SpriteEditorSprite.java
+++ code/src/game/graphics/gui/editors/SpriteEditorSprite.java
@@ -9,12 +9,11 @@ import game.network.packet.editors.EditorData;
 import graphics.gl00.Context;
 
 public class SpriteEditorSprite extends Sprite {
-  private int _spriteCRC;
   protected ArrayList<Frame> _frame = super._frame;
   protected ArrayList<Anim>  _anim  = super._anim;
   
-  public SpriteEditorSprite(String file) { this(file, false); }
-  public SpriteEditorSprite(String file, boolean newData) {
+  protected SpriteEditorSprite(String file) { this(file, false); }
+  protected SpriteEditorSprite(String file, boolean newData) {
     super(file);
     
     if(!newData) {
@@ -25,18 +24,14 @@ public class SpriteEditorSprite extends Sprite {
     }
   }
   
-  public boolean isChanged() {
-    return _spriteCRC != serialize().crc();
-  }
+  protected String getTexture() { return _texture; }
   
-  public String getTexture() { return _texture; }
-  
-  public void setName(String name) { _name = name; }
-  public void setNote(String note) { _note = note; }
-  public void setW(int w) { _w = w; }
-  public void setH(int h) { _h = h; }
-  public void setScript(String script) { _script = script; }
-  public void setTexture(String texture) { _texture = texture; }
+  protected void setName(String name) { _name = name; }
+  protected void setNote(String note) { _note = note; }
+  protected void setW(int w) { _w = w; }
+  protected void setH(int h) { _h = h; }
+  protected void setScript(String script) { _script = script; }
+  protected void setTexture(String texture) { _texture = texture; }
   
   public void request() {
     EditorData.Request p = new EditorData.Request(this);
@@ -63,11 +58,11 @@ public class SpriteEditorSprite extends Sprite {
     });
   }
   
-  public void serializeInternal(Buffer b) {
+  protected void serializeInternal(Buffer b) {
     super.serializeInternal(b);
   }
   
-  public void deserializeInternal(Buffer b) {
+  protected void deserializeInternal(Buffer b) {
     super.deserializeInternal(b);
   }
 }
