23449dc3b7c9616f9e9b65cb9a957b3c21a4decc
Corey Frenette <corey@narwhunderful.com>
2013-08-08 17:14:32 -0300

    EntityAttack received from server

--- code/src/game/network/packet/EntityAttack.java
+++ code/src/game/network/packet/EntityAttack.java
@@ -1,5 +1,7 @@
 package game.network.packet;
 
+import game.Game;
+import game.world.Entity;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import network.packet.Packet;
@@ -7,9 +9,17 @@ import network.packet.Packet;
 public class EntityAttack extends Packet {
   private double _angle;
   
+  private Entity _attacker, _defender;
+  private int _damage;
+  
+  public EntityAttack() { }
   public EntityAttack(double angle) {
     _angle = angle;
   }
+  
+  public Entity attacker() { return _attacker; }
+  public Entity defender() { return _defender; }
+  public int damage() { return _damage; }
   
   public int getIndex() {
     return 39;
@@ -22,7 +32,13 @@ public class EntityAttack extends Packet {
   }
   
   public void deserialize(ByteBuf data) throws NotEnoughDataException {
+    _attacker = Game.getInstance().getWorld().getEntity(data.readInt());
     
+    int defender = data.readInt();
+    if(defender != 0) {
+      _defender = Game.getInstance().getWorld().getEntity(defender);
+      _damage = data.readInt();
+    }
   }
   
   public void process() {
